8649e1d93da77b720fce7a2634adbd35177e8f70
losTripleG <parragaspar2@gmail.com>
2025-05-14 15:13:48 -0300

    HOLA

--- Inicio.py
+++ Inicio.py
@@ -4,7 +4,6 @@ import streamlit as st
 # --- Page Configuration (Optional but Recommended) ---
 st.set_page_config(
     page_title="Insulink",
-    page_title="Insulink - Login",
     page_icon="💉",
     layout="centered" # "wide" or "centered"
 )
