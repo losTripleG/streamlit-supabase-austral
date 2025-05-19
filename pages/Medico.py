503d41a8317c84efa8448e5d067010ab6d3014ec
gcogliati <giulicogliati@gmail.com>
2025-05-19 12:11:22 -0300

    Update Medico.py

--- pages/Medico.py
+++ pages/Medico.py
@@ -31,41 +31,6 @@ st.title("Médico")
 ##############################################################
 
 st.markdown("""
-    <h3 style='text-align: wide; font-size: 24px;'>¿Deseas registrarte?</h3>
-""", unsafe_allow_html=True)
-
-
-# Check if the user is already signed in (using session state)
-if not st.session_state.get("signed_in", False):
-    # If not signed in, show the sign in form
-    with st.form("signin_form"):
-        username = st.text_input("Nombre de usuario")
-        password = st.text_input("Contraseña", type="password")
-        submitted = st.form_submit_button("Registrarse")
-
-        if submitted:
-            # For this demo, any username/password is accepted
-            if username and password:
-                st.session_state["signed_in"] = True
-                st.session_state["username"] = username # Optional: store username
-                st.success("Usuario registrado correctamente! 😃")
-            else:
-                st.error("Por favor, ingrese nombre de usuario y contraseña. 😠")
-else:
-    # If signed in, show a welcome message
-    st.success(f"Welcome, {st.session_state.get('username', 'User')}!")
-    st.info("Navigate using the sidebar on the left to manage different sections.")
-    #st.balloons() # Fun little animation
-
-    # Optional: Add a logout button
-    if st.button("Logout"):
-        del st.session_state["logged_in"]
-        if "username" in st.session_state:
-            del st.session_state["username"]
-
-##################################################################
-
-st.markdown("""
     <h3 style='text-align: wide; font-size: 24px;'>¿Deseas iniciar sesión?</h3>
 """, unsafe_allow_html=True)
 # Check if the user is already logged in (using session state)
